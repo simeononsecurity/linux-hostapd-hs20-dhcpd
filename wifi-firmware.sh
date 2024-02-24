86e1884be2f8a011e71c7d89928fdec3cbd55a4a
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2024-02-24 06:34:07 -0600

    Update wifi-firmware.sh

--- wifi-firmware.sh
+++ wifi-firmware.sh
@@ -22,6 +22,26 @@ declare -A firmware_urls=(
             https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/tree/rtw88/rtw8822b_fw.bin
             https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/tree/rtw88/rtw8822c_fw.bin
             https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/tree/rtw88/rtw8822c_wow_fw.bin"
+    [mt7996]="https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mediatek/mt7996/mt7996_dsp.bin
+              https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mediatek/mt7996/mt7996_eeprom.bin
+              https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mediatek/mt7996/mt7996_rom_patch.bin
+              https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mediatek/mt7996/mt7996_wa.bin
+              https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mediatek/mt7996/mt7996_wm.bin"
+    [mt7986]="https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mediatek/mt7986/mt7986_wo_1.bin
+              https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mediatek/mt7986/mt7986_wo_0.bin
+              https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mediatek/mt7986/mt7986_wm_mt7975.bin
+              https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mediatek/mt7986/mt7986_wm.bin
+              https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mediatek/mt7986/mt7986_wa.bin
+              https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mediatek/mt7986/mt7986_rom_patch_mt7975.bin
+              https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mediatek/mt7986/mt7986_rom_patch.bin
+              https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mediatek/mt7986/mt7986_eeprom_mt7976_dual.bin
+              https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mediatek/mt7986/mt7986_eeprom_mt7976_dbdc.bin
+              https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mediatek/mt7986/mt7986_eeprom_mt7976.bin
+              https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mediatek/mt7986/mt7986_eeprom_mt7975_dual.bin"
+    [mt7981]="https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mediatek/mt7981/mt7981_wo.bin
+              https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mediatek/mt7981/mt7981_wm.bin
+              https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mediatek/mt7981/mt7981_wa.bin
+              https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mediatek/mt7981/mt7981_rom_patch.bin"
 )
 
 declare -A destination_dirs=(
@@ -30,6 +50,9 @@ declare -A destination_dirs=(
     [mt7961]="/lib/firmware/mediatek"
     [mt7610]="/lib/firmware/mediatek"
     [rtw88]="/lib/firmware/rtw88"
+    [mt7996]="/lib/firmware/mediatek/mt7996"
+    [mt7986]="/lib/firmware/mediatek/mt7986"
+    [mt7981]="/lib/firmware/mediatek/mt7981"
 )
 
 # Function to download and copy firmware files for a given device
